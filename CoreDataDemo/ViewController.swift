19b3b1243bc9fd191389cf0de830cf37d81521d4
ANHTT-D1 <anhtt@rikkeisoft.com>
2018-07-24 14:46:22 +0700

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by ANHTT-D1 on 7/24/18.
+//  Copyright © 2018 RikkeiIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
